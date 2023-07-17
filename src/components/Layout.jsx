574dc0c3acec4707b270a92b35e0449654718ebc
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-07-17 16:40:14 +0200

    fix:navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navbar from '@/components/Navbar'
+// 
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar />
+  
       <div>{children}</div>
     </>
   );
