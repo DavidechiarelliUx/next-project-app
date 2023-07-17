8809b605d572da4b320f54fa54b87b9f1c3a08ef
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-07-17 16:01:45 +0200

    fix : error navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Navbar from '@/components/Navbar';
+// import Navbar from '@/components/Navbar';
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <div>{children}</div>
-      {/* Aggiungi qui il tuo footer se lo desideri */}
     </>
   );
 };
